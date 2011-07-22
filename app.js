d3e9d9e3be9f7f73163a6d3c7b6b91d472cffdc8
Isaac Sanders <isaac@edgecase.com>
2011-07-21 21:45:03 -0400

    Fidgeted to work with heroku

--- app.js
+++ app.js
@@ -34,5 +34,7 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
